49d6e0969f174202bc32d7c9df542cec1bedc7c4
chicotemaa <chicote.matias@gmail.com>
2026-03-09 10:44:30 -0300

    fix(build): marcar budget-module como client component

--- components/projects/budget/budget-module.tsx
+++ components/projects/budget/budget-module.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useCallback, useEffect, useState } from "react";
 import { BudgetVersion, BudgetItem } from "@/lib/types/budget";
 import { BudgetStats } from "./budget-stats";
